46febcceea020a77fd01d42d19aa3dc8d2ed11b2
Sander Steeghs-Turchina <sander@steeghs.info>
2025-06-30 12:44:47 +0200

    add QKOTC test

--- test/solver/test_scramble_solver.py
+++ test/solver/test_scramble_solver.py
@@ -4,6 +4,7 @@ from scramble.core import Level, Court, Player, HistoryManager
 from scramble.solver import ScrambleSolver
 from scramble.settings import Settings
 
+
 def get_solver(num_players: int, num_courts: int):
     players = []
     for i in range(num_players):
@@ -13,7 +14,7 @@ def get_solver(num_players: int, num_courts: int):
 
     courts = []
     for i in range(num_courts):
-        name = "Court {i+1}"
+        name = f"Court {i+1}"
         court = Court(name, id=f"{i+1}")
         courts.append(court)
 
@@ -21,6 +22,7 @@ def get_solver(num_players: int, num_courts: int):
     settings = Settings()
     solver = ScrambleSolver(players, history, courts, settings)
     return solver
+
 
 @pytest.mark.parametrize("num_matches", [
     pytest.param(1),
@@ -30,7 +32,7 @@ def get_solver(num_players: int, num_courts: int):
 ])
 @pytest.mark.timeout(60)
 def test_n_matches_no_history_same_level(num_matches):
-    solver = get_solver(num_matches * 4, num_matches)
+    solver = get_solver(num_players=num_matches * 4, num_courts=num_matches)
     round = solver.solve()
 
     assert len(round.matches) == num_matches
@@ -38,3 +40,11 @@ def test_n_matches_no_history_same_level(num_matches):
     for i in range(num_matches):
         assert len(round.matches[i].all_player_ids()) == 4
         assert len(round.matches[i].teams) == 2
+
+@pytest.mark.timeout(60)
+@pytest.mark.skip(reason="RuntimeError: No feasible solution found")
+def test_1_team_qkotc_1_team_match():
+    solver = get_solver(num_players=10, num_courts=2)
+    round = solver.solve()
+
+    assert len(round.matches) == 2
