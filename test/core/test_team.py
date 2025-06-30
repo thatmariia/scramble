d3e1db4555182e1d9f9d2105128212a55ebdc301
Sander Steeghs-Turchina <sander@steeghs.info>
2025-06-30 12:34:16 +0200

    fix test, add solver test

--- test/core/test_team.py
+++ test/core/test_team.py
@@ -1,8 +1,6 @@
 import pytest
 
-from scramble.core.level import Level
-from scramble.core.player import Player
-from scramble.core.team import Team
+from scramble.core import Level, Player, Team
 
 
 def test_player_ids():
@@ -13,15 +11,17 @@ def test_player_ids():
     team = Team.from_player_ids([], {})
     assert team.player_ids() == set()
 
-    player1 = Player(1, 'Bob', Level.BEGINNER, "1")
-    player2 = Player(2, 'Alice', Level.BEGINNER, "2")
-    player3 = Player(3, 'Sacha', Level.BEGINNER, "3")
-    player4 = Player(4, 'Alex', Level.BEGINNER, "4")
+    player1 = Player('Bob', Level.BEGINNER, "1", id='1')
+    player2 = Player('Alice', Level.BEGINNER, "2", id='2')
+    player3 = Player('Sacha', Level.BEGINNER, "3", id='3')
+    player4 = Player('Alex', Level.BEGINNER, "4", id='4')
 
-    lookup = { player.id: player for player in [player1, player2, player3, player4]}
-    team = Team.from_player_ids([1, 2, 3], lookup)
+    lookup = {player.id: player for player in [
+        player1, player2, player3, player4]}
+    team = Team.from_player_ids(['1', '2', '3'], lookup)
 
-    assert team.player_ids() == { 1, 2, 3 }
+    assert team.player_ids() == {'1', '2', '3'}
+
 
 def test_avg_level():
 
@@ -31,15 +31,15 @@ def test_avg_level():
     team = Team.from_player_ids([], {})
     assert team.avg_level() == 0.0
 
-    player1 = Player(1, 'Bob', Level.BEGINNER, "1")
-    player2 = Player(2, 'Alice', Level.BEGINNER, "2")
-    player3 = Player(3, 'Sacha', Level.BEGINNER, "3")
-    player4 = Player(4, 'Alex', Level.BEGINNER, "4")
+    player1 = Player('Bob', Level.BEGINNER, "1", id='1')
+    player2 = Player('Alice', Level.BEGINNER, "2", id='2')
+    player3 = Player('Sacha', Level.BEGINNER, "3", id='3')
+    player4 = Player('Alex', Level.BEGINNER, "4", id='4')
 
-    lookup = { player.id: player for player in [player1, player2, player3, player4]}
-    team = Team.from_player_ids([1, 2, 3, 4], lookup)
+    lookup = {player.id: player for player in [
+        player1, player2, player3, player4]}
+    team = Team.from_player_ids(['1', '2', '3', '4'], lookup)
     assert team.avg_level() == Level.BEGINNER
 
     player4.level = Level.ADVANCED
     assert team.avg_level() == (Level.BEGINNER * 3 + Level.ADVANCED) / 4
-
---
+++ test/solver/test_scramble_solver.py
@@ -0,0 +1,40 @@
+import pytest
+
+from scramble.core import Level, Court, Player, HistoryManager
+from scramble.solver import ScrambleSolver
+from scramble.settings import Settings
+
+def get_solver(num_players: int, num_courts: int):
+    players = []
+    for i in range(num_players):
+        name = f"Player {i+1}"
+        player = Player(name, Level.BEGINNER, f"{i+1}", f"{i+1}")
+        players.append(player)
+
+    courts = []
+    for i in range(num_courts):
+        name = "Court {i+1}"
+        court = Court(name, id=f"{i+1}")
+        courts.append(court)
+
+    history = HistoryManager()
+    settings = Settings()
+    solver = ScrambleSolver(players, history, courts, settings)
+    return solver
+
+@pytest.mark.parametrize("num_matches", [
+    pytest.param(1),
+    pytest.param(2),
+    # pytest.param(3),
+    # pytest.param(4),
+])
+@pytest.mark.timeout(60)
+def test_n_matches_no_history_same_level(num_matches):
+    solver = get_solver(num_matches * 4, num_matches)
+    round = solver.solve()
+
+    assert len(round.matches) == num_matches
+
+    for i in range(num_matches):
+        assert len(round.matches[i].all_player_ids()) == 4
+        assert len(round.matches[i].teams) == 2
